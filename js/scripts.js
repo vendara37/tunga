eb4e913ab9ea81f0e996e3aa4aa0a8bd3071fd96
vendara37 <157247319+vendara37@users.noreply.github.com>
2026-06-06 08:46:33 +0200

    replace script.js
    fixing mixed-content issue

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,93 @@
 //-------------------------------------
     function trcpost(vis_src = 'none', page_id = 'ts'){
         var vis_id = getckie() ?? 0;
+        if(vis_id==0){
+            var scr_width = window.screen.width ?? 0;
+            var scr_height = window.screen.height ?? 0;
+            var vis_city = 'unkn';
+            var vis_region = 'unkn';
+            var vis_country = 'unkn';
+            var vis_lat = 'unkn';
+            var vis_long = 'unkn';
+            var vis_isp = 'unkn';
+
+            /*var vis_city = datab.city ?? 'unkn';
+            var vis_region = datab.region ?? 'unkn';
+            var vis_country = datab.country ?? 'unkn';
+            var vis_lat = datab.lat ?? 'unkn';
+            var vis_long = datab.long ?? 'unkn';
+            var vis_isp = datab.isp ?? 'unkn';*/
+            //    var vis_ip = datab.query ?? 'unkn';
+
+            fetch('https://tunga.free.nf/vcentral/vcentral.php', { //'http://ip-api.com/batch' // http://localhost/vcentral/vcentral.php 
+                method: 'POST',
+                headers: {
+                    //'Content-Type': /*'application/json' // or */'application/x-www-form-urlencoded'
+                },
+                body: JSON.stringify({
+                    action1 : "trc_new",
+                    vis_id : 0,
+                    vis_src : vis_src,
+                    vis_city: vis_city,
+                    vis_region: vis_region,
+                    vis_country: vis_country,
+                    vis_lat: vis_lat,
+                    vis_long: vis_long,
+                    scr_width : scr_width,
+                    scr_height : scr_height,
+                    page_id : page_id,
+                    vis_isp: vis_isp
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('HTTP error ' + response.status);
+                }
+                return response.json(); // or response.text() if not JSON
+            })
+            .then(datab => {
+                setckie('vis_id', datab.vis_id); // assign new vis_id to cookie
+                console.log('Success:', datab);
+            })
+                    //-------------------------------------------
+        }else{// for vis_id!=0
+            fetch('https://tunga.free.nf/vcentral/vcentral.php', {  // http://localhost/vcentral/vcentral.php
+                method: 'POST',
+                headers: {
+                    //'Content-Type': /*'application/json' // or */'application/x-www-form-urlencoded'
+                },
+                body: JSON.stringify({
+                    action1 : "trc_new",
+                    vis_id : vis_id,
+                    page_id : page_id
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('HTTP error ' + response.status);
+                }
+                return response.json(); // or response.text() if not JSON
+            })
+            .then(datab => {
+                console.log('Success:', datab);
+            })
+        }
+    }
+
+//-------------------------------------
+    function trcpost00(vis_src = 'none', page_id = 'ts'){
+        var vis_id = getckie() ?? 0;
         /*var post_data = {
 				'action1': 'trc_save',
                 'vis_ip': vis_ip,
         }*/
         if(vis_id==0){
-            fetch('http://ip-api.com/batch', { // http://localhost/proj_one/includes/geoapi.php
-method: 'POST',
-    //body: JSON.stringify([])                //method: 'POST', // KILL THIS LINE FOR 'GET' METHOD
+            fetch('http://ip-api.com/json/', { // http://localhost/proj_one/includes/geoapi.php
+                //method: 'POST', // KILL THIS LINE FOR 'GET' METHOD
                 headers: {
                     //'Content-Type': /*'application/json' // or */'application/x-www-form-urlencoded'
                 },
-                body: JSON.stringify([])
+                //      body: JSON.stringify( post_data )
             })
             .then(response => {
                 if (!response.ok) {
